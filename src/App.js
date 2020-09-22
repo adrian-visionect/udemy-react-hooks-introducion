0465a73290ffa6b4f92fd8ebb171b59a88eb2c11
adtokarz <adrian.tokarz@visionect.io>
2020-09-22 15:19:16 +0200

    create

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [isActive, setIsActive] = useState(true);
+
+  // const handler = () => {
+  //   setCounter(counter + 1);
+  // };
+
+  const toggleVisiblityCounter = () => {
+    setIsActive((prevValue) => !prevValue);
+  };
+
+  // useEffect(() => {}, []);
+
+  const counterComponent = isActive ? <Counter /> : null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={toggleVisiblityCounter}>Pokaż/ukryj komponent</button>
+      {counterComponent}
     </div>
   );
-}
+};
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleMouseMove = (event) => setCounter(event.clientX);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  });
+
+  return (
+    <div>
+      <p>{counter}</p>
+    </div>
+  );
+};
 
 export default App;
